c8fc468456ddb1bc2e2f3f146a0cf734e012b957
abdelrahman mohamed <abdelrahmanmohamed@abdelrahmans-MacBook-Pro.local>
2015-07-24 19:04:07 +0200

    final commit

---
+++ Record Teen/PlaySoundViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PlaySoundViewControlerViewController.swift
+//  Record Teen
+//
+//  Created by abdelrahman mohamed on 7/16/15.
+//  Copyright (c) 2015 abdelrahman mohamed. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundViewControlerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ RecordedAudio.swift
@@ -0,0 +1,9 @@
+//
+//  RecordedAudio.swift
+//  Record Teen
+//
+//  Created by abdelrahman mohamed on 7/19/15.
+//  Copyright (c) 2015 abdelrahman mohamed. All rights reserved.
+//
+
+import Foundation
